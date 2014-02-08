969d6cea6124c22e1b71e7955eb354816373477e
Nicolas OGIER <nicolas.ogier@joomla.fr>
2014-02-08 19:48:39 +0100

    Correction de l'access et l'ordering #__languages (joomla.sql)

--- Installation-fr-FR/sql/mysql/joomla_fr.sql
+++ Installation-fr-FR/sql/mysql/joomla_fr.sql
@@ -1317,8 +1317,8 @@ CREATE TABLE IF NOT EXISTS `#__languages` (
 --
 
 INSERT INTO `#__languages` (`lang_id`, `lang_code`, `title`, `title_native`, `sef`, `image`, `description`, `metakey`, `metadesc`, `sitename`, `published`, `access`, `ordering`) VALUES
-(1, 'en-GB', 'English (UK)', 'English (UK)', 'en', 'en', '', '', '', '', 1, 0, 1),
-(2, 'fr-FR', 'Français (FR)', 'Français (FR)', 'fr', 'fr', '', '', '', '', 1, 0, 0);
+(1, 'en-GB', 'English (UK)', 'English (UK)', 'en', 'en', '', '', '', '', 1, 1, 2),
+(2, 'fr-FR', 'Français (FR)', 'Français (FR)', 'fr', 'fr', '', '', '', '', 1, 1, 1);
 
 
 -- --------------------------------------------------------
